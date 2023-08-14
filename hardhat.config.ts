78d29268aaeba78c9cba085d466836426fadbef6
RyanFcr <3200103626@zju.edu.cn>
2023-08-14 01:24:05 -0400

    feat: baseline

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+// import ("@nomiclabs/hardhat-etherscan")
+import "dotenv/config"
+
+
+
+let SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+let PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+let ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  solidity: "0.8.7",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [
+        PRIVATE_KEY
+      ],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
+
 
 export default config;
