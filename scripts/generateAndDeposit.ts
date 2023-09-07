1589798c7181eaca38cf961aa2e527c40e985e68
RyanFcr <3200103626@zju.edu.cn>
2023-09-07 14:44:03 -0400

    feat: baseline

--- scripts/generateAndDeposit.ts
+++ scripts/generateAndDeposit.ts
@@ -1,17 +1,19 @@
 import { ethers } from "hardhat"
-import crypto from "crypto"
 
 async function main() {
     const stakerWallets: any[] = []
-    const userWallet: any[] = []
     const N = 10
 
-    const wallet = ethers.Wallet.createRandom()
-    userWallet.push({
-        address: wallet.address,
-        privateKey: wallet.privateKey,
-        publicKey: wallet.publicKey,
-    })
+    const userPrivateKey = process.env.PRIVATE_KEY
+
+    if (!userPrivateKey) {
+        console.error("Private key not found in .env file")
+        return
+    }
+
+    const userWallet = new ethers.Wallet(userPrivateKey, ethers.provider)
+
+    console.log("Address:", userWallet.address)
 
     for (let i = 0; i <= N; i++) {
         // N is the number of stakers
@@ -31,6 +33,8 @@ async function main() {
     //DAI：USDT = 1:1
     //BUY(Y, X, Δr_Y, Δr_X, s, md)=SwapTokensforExactTokens
     //SELL(X, Y, Δr_X, Δr_Y, s, md)=SwapExactTokensForTokens
+
+    //Uniswap-V2 router on sepolia: 0x86dcd3293C53Cf8EFd7303B57beb2a3F671dDE98
 }
 
 main().catch((error) => {
