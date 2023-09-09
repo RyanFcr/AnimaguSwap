9580f8c1839617b822fbdd23eb116418fa1fb4d2
RyanFcr <3200103626@zju.edu.cn>
2023-09-09 16:36:16 -0400

    feat: baseline

--- scripts/generateAndDeposit.ts
+++ scripts/generateAndDeposit.ts
@@ -1,16 +1,5 @@
 import { ethers } from "hardhat"
 import * as fs from "fs"
-import {
-    ChainId,
-    Fetcher,
-    Percent,
-    Route,
-    Token,
-    TokenAmount,
-    Trade,
-    TradeType,
-    WETH,
-} from "@uniswap/sdk"
 import { Contract } from "ethers"
 
 async function main() {
@@ -27,17 +16,8 @@ async function main() {
     const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ""
     const Goerli_RPC_URL = process.env.GOERLI_RPC_URL || ""
     // const provider = new ethers.JsonRpcProvider(SEPOLIA_RPC_URL)
-    const provider = new ethers.JsonRpcProvider(Goerli_RPC_URL)
+    const provider = new ethers.JsonRpcProvider(SEPOLIA_RPC_URL)
     const userWallet = new ethers.Wallet(userPrivateKey, provider)
-
-    enum ExtendedChainId {
-        SEPOLIA = 11155111,
-        MAINNET = ChainId.MAINNET,
-        ROPSTEN = ChainId.ROPSTEN,
-        RINKEBY = ChainId.RINKEBY,
-        GÖRLI = ChainId.GÖRLI,
-        KOVAN = ChainId.KOVAN,
-    }
 
     // console.log("Address:", userWallet.address)
 
@@ -83,28 +63,61 @@ async function main() {
     //Uniswap-V2 router on goerli: 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
 
     // Define Uniswap V2 router contract
-    const UNISWAP_ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
-    const UNISWAP_ROUTER_ABI = JSON.parse(
+    const IUniswapV2Router02_ADDRESS =
+        "0x86dcd3293C53Cf8EFd7303B57beb2a3F671dDE98"
+    const IUniswapV2Router02_ABI = JSON.parse(
         fs.readFileSync("./abis/router.json").toString(),
     )
-    const routerContract = new Contract(
-        UNISWAP_ROUTER_ADDRESS,
-        UNISWAP_ROUTER_ABI,
+    const IUniswapV2Router02 = new Contract(
+        IUniswapV2Router02_ADDRESS,
+        IUniswapV2Router02_ABI,
         userWallet,
     )
+    // const IUniswapV2Factory_ADDRESS =
+    //     "0xc9f18c25Cfca2975d6eD18Fc63962EBd1083e978"
+    // const IUniswapV2Factory_ABI = JSON.parse(
+    //     fs.readFileSync("./abis/IUniswapV2Factory.json").toString(),
+    // )
+    // const IUniswapV2Factory = new Contract(
+    //     IUniswapV2Factory_ADDRESS,
+    //     IUniswapV2Factory_ABI,
+    //     userWallet,
+    // )
+
+    // const IUniswapV2Pair_ADDRESS = "0xc9f18c25Cfca2975d6eD18Fc63962EBd1083e978"
+    // const IUniswapV2Pair_ABI = JSON.parse(
+    //     fs.readFileSync("./abis/IUniswapV2Pair.json").toString(),
+    // )
+    // const IUniswapV2Pair = new Contract(
+    //     IUniswapV2Pair_ADDRESS,
+    //     IUniswapV2Pair_ABI,
+    //     userWallet,
+    // )
 
     //Sepolia DAI 0x64cE2F75c6887C77c61991bA2D6e456f9698adc3
     //Goerli DAI 0x5c221e77624690fff6dd741493d735a17716c26b
-    const DAI: Token = new Token(
-        ChainId.GÖRLI,
-        "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD",
-        18,
-    )
-    // const DAI_ABI = JSON.parse(fs.readFileSync("./abis/erc20.json").toString())
-    // Generate a random number
-    // const random = Math.round(Math.random())
+    // Uni 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984
+    // const Uni: Token = new Token(
+    //     ExtendedChainId.SEPOLIA as any,
+    //     "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+    //     18,
+    // )
 
-    const random = 0
+    const UNI_ADDRESS = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"
+    const UNI_ABI = JSON.parse(fs.readFileSync("./abis/erc20.json").toString())
+    const UNI_CONTRACT = new ethers.Contract(UNI_ADDRESS, UNI_ABI, userWallet)
+
+    const WETH_SEPOLIA_ADDRESS = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14"
+    // Generate a random number
+    // const pairAddress = await IUniswapV2Factory.getPair(
+    //     WETH_SEPOLIA_ADDRESS,
+    //     UNI_ADDRESS,
+    // )
+    // console.log("ETH-UNI pair address:", pairAddress)
+
+    const random = Math.round(Math.random())
+
+    // const random = 1
     console.log("Random number:", random)
 
     let balance = await provider.getBalance(userWallet.address)
@@ -114,73 +127,8 @@ async function main() {
         )}`,
     )
 
-    async function swapTokens(
-        token1: Token,
-        token2: Token,
-        amount: string,
-        slippage: string = "50",
-    ) {
-        try {
-            const pair = await Fetcher.fetchPairData(token1, token2)
-            const route = new Route([pair], token2)
-
-            const amountIn = BigInt(ethers.parseEther(amount).toString()) // Convert to BigInt
-
-            const slippageTolerance = new Percent(slippage, "1000000")
-
-            const trade = new Trade(
-                route,
-                new TokenAmount(token2, amountIn.toString()),
-                TradeType.EXACT_INPUT,
-            )
-
-            const amountOutMin = BigInt(
-                trade.minimumAmountOut(slippageTolerance).raw.toString(),
-            )
-
-            const path = [token2.address, token1.address]
-            const to = userWallet.address
-
-            const deadline = BigInt(Math.floor(Date.now() / 1000) + 60 * 20)
-
-            const value = BigInt(trade.inputAmount.raw.toString())
-
-            const rawTxn = await routerContract.swapExactETHForTokens(
-                amountOutMin.toString(),
-                path,
-                to,
-                deadline.toString(),
-                {
-                    value: value.toString(), // Converted BigInt to string
-                },
-            )
-
-            const sendTxn = await userWallet.sendTransaction(rawTxn)
-            const receipt = await sendTxn.wait()
-
-            if (receipt) {
-                console.log(
-                    " - Transaction is mined - ",
-                    "Transaction Hash:",
-                    sendTxn.hash,
-                    "Block Number:",
-                    receipt.blockNumber,
-                    "Navigate to https://sepolia.etherscan.io/txn/" +
-                        sendTxn.hash,
-                    "to see your transaction",
-                )
-            } else {
-                console.log("Error submitting transaction")
-            }
-        } catch (e) {
-            console.log(e)
-        }
-    }
-    if (random === 0) {
+    if (random == 0) {
         // Use ETH to buy DAI
-        // const WETH_SEPOLIA_ADDRESS =
-        //     "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9"
-
         // const WETH_EXTENDED: { [chainId in ExtendedChainId]: Token } = {
         //     ...WETH,
         //     [ExtendedChainId.SEPOLIA]: new Token(
@@ -189,39 +137,52 @@ async function main() {
         //         18,
         //     ),
         // }
-
-        swapTokens(WETH[ChainId.GÖRLI], DAI, "0.001")
-        // const amountIn = ethers.parseUnits("0.01") // amount of ETH you want to send
-        // const amountOutMin = ethers.parseUnits("0.005") // set minimum amount of DAI you want to receive, better to use a realistic value here
-        // const deadline = Math.floor(Date.now() / 1000) + 60 * 10 // 10 minutes from now
-
+        const amountIn = ethers.parseUnits("0.01") // amount of ETH you want to send
+        const amountOutMin = ethers.parseUnits("0.005") // set minimum amount of DAI you want to receive, better to use a realistic value here
+        const path = [WETH_SEPOLIA_ADDRESS, UNI_ADDRESS]
+        const deadline = Math.floor(Date.now() / 1000) + 60 * 10 // 10 minutes from now
         // // Call swapExactETHForTokens
-        // const tx = await routerContract.swapExactETHForTokens(
-        //     amountOutMin,
-        //     [WETH_SEPOLIA_ADDRESS, DAI.address],
-        //     userWallet.address,
-        //     deadline,
-        //     { value: amountIn },
-        // )
+        const tx = await IUniswapV2Router02.swapExactETHForTokens(
+            amountOutMin,
+            path,
+            userWallet.address,
+            deadline,
+            {
+                value: amountIn,
+                gasLimit: 300000,
+            },
+        )
+        console.log("Transaction hash:", tx.hash)
+        const receipt = await tx.wait()
+        console.log("Transaction was mined in block:", receipt.blockNumber)
     } else {
-        // Use DAI to buy a specific amount of ETH
-        // const amountOutExact = ethers.parseUnits("0.001", 18) // specify exact amount of ETH you want to receive
-        // const amountInMax = ethers.parseUnits("12", 18) // specify maximum amount of DAI you're willing to pay
-        // const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from now
-        // // First, we need to approve the router to spend our DAI
-        // const daiContract = new Contract(DAI.address, DAI_ABI, userWallet)
-        // await daiContract.approve(UNISWAP_ROUTER_ADDRESS, amountInMax)
-        // // Call swapTokensForExactETH
-        // const tx = await routerContract.swapTokensForExactETH(
-        //     amountOutExact,
-        //     amountInMax,
-        //     [DAI.address, WETH_SEPOLIA_ADDRESS],
-        //     userWallet.address,
-        //     deadline,
-        // )
-        // console.log("Transaction hash:", tx.hash)
-        // await tx.wait()
-        // console.log("Swap done!")
+        // Use Uni to buy ETH
+        const amountIn = ethers.parseUnits("10", 18) // amount of UNI you want to sell, e.g., 10 UNI
+        const amountOutMin = ethers.parseUnits("0.005", 18) // set minimum amount of ETH you want to receive
+        const path = [UNI_ADDRESS, WETH_SEPOLIA_ADDRESS]
+        const deadline = Math.floor(Date.now() / 1000) + 60 * 10 // 10 minutes from now
+
+        // Approve the Uniswap Router to spend your UNI tokens.
+        const approvalTx = await UNI_CONTRACT.approve(
+            IUniswapV2Router02_ADDRESS,
+            amountIn,
+        )
+        await approvalTx.wait()
+
+        // Call swapExactTokensForETH
+        const tx = await IUniswapV2Router02.swapExactTokensForETH(
+            amountIn,
+            amountOutMin,
+            path,
+            userWallet.address,
+            deadline,
+            {
+                gasLimit: 300000,
+            },
+        )
+        console.log("Transaction hash:", tx.hash)
+        const receipt = await tx.wait()
+        console.log("Transaction was mined in block:", receipt.blockNumber)
     }
 }
 
