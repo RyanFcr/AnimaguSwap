b93aa7f3aaeb15938342c01582cf472a400c810e
RyanFcr <3200103626@zju.edu.cn>
2023-09-10 17:59:27 -0400

    feat: stage1 generate

--- scripts/generateAndDeposit.ts
+++ scripts/generateAndDeposit.ts
@@ -2,6 +2,17 @@ import { ethers } from "hardhat"
 import * as fs from "fs"
 import { Contract } from "ethers"
 
+function randomBit(): number {
+    // Create a Uint8Array with a length of 1
+    let arr = new Uint8Array(1)
+
+    // Populate the array with random values
+    crypto.getRandomValues(arr)
+
+    // Return the least significant bit of the first element (i.e., 0 or 1)
+    return arr[0] & 1
+}
+
 async function main() {
     const stakerWallets: any[] = []
     const N = 2
@@ -15,11 +26,8 @@ async function main() {
 
     const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ""
     const Goerli_RPC_URL = process.env.GOERLI_RPC_URL || ""
-    // const provider = new ethers.JsonRpcProvider(SEPOLIA_RPC_URL)
     const provider = new ethers.JsonRpcProvider(SEPOLIA_RPC_URL)
     const userWallet = new ethers.Wallet(userPrivateKey, provider)
-
-    // console.log("Address:", userWallet.address)
 
     for (let i = 1; i <= N; i++) {
         // N is the number of stakers
@@ -73,27 +81,6 @@ async function main() {
         IUniswapV2Router02_ABI,
         userWallet,
     )
-    // const IUniswapV2Factory_ADDRESS =
-    //     "0xc9f18c25Cfca2975d6eD18Fc63962EBd1083e978"
-    // const IUniswapV2Factory_ABI = JSON.parse(
-    //     fs.readFileSync("./abis/IUniswapV2Factory.json").toString(),
-    // )
-    // const IUniswapV2Factory = new Contract(
-    //     IUniswapV2Factory_ADDRESS,
-    //     IUniswapV2Factory_ABI,
-    //     userWallet,
-    // )
-
-    // const IUniswapV2Pair_ADDRESS = "0xc9f18c25Cfca2975d6eD18Fc63962EBd1083e978"
-    // const IUniswapV2Pair_ABI = JSON.parse(
-    //     fs.readFileSync("./abis/IUniswapV2Pair.json").toString(),
-    // )
-    // const IUniswapV2Pair = new Contract(
-    //     IUniswapV2Pair_ADDRESS,
-    //     IUniswapV2Pair_ABI,
-    //     userWallet,
-    // )
-
     //Sepolia DAI 0x64cE2F75c6887C77c61991bA2D6e456f9698adc3
     //Goerli DAI 0x5c221e77624690fff6dd741493d735a17716c26b
     // Uni 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984
@@ -108,18 +95,12 @@ async function main() {
     const UNI_CONTRACT = new ethers.Contract(UNI_ADDRESS, UNI_ABI, userWallet)
 
     const WETH_SEPOLIA_ADDRESS = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14"
-    // Generate a random number
-    // const pairAddress = await IUniswapV2Factory.getPair(
-    //     WETH_SEPOLIA_ADDRESS,
-    //     UNI_ADDRESS,
-    // )
-    // console.log("ETH-UNI pair address:", pairAddress)
 
-    const random = Math.round(Math.random())
+    const random = randomBit()
+    const B = randomBit()
 
-    // const random = 1
     console.log("Random number:", random)
-
+    console.log("B:", B)
     let balance = await provider.getBalance(userWallet.address)
     console.log(
         `User wallet balance:${parseFloat(ethers.formatEther(balance)).toFixed(
@@ -127,63 +108,127 @@ async function main() {
         )}`,
     )
 
-    if (random == 0) {
-        // Use ETH to buy DAI
-        // const WETH_EXTENDED: { [chainId in ExtendedChainId]: Token } = {
-        //     ...WETH,
-        //     [ExtendedChainId.SEPOLIA]: new Token(
-        //         ExtendedChainId.SEPOLIA as any,
-        //         WETH_SEPOLIA_ADDRESS,
-        //         18,
-        //     ),
-        // }
-        const amountIn = ethers.parseUnits("0.01") // amount of ETH you want to send
-        const amountOutMin = ethers.parseUnits("0.005") // set minimum amount of DAI you want to receive, better to use a realistic value here
-        const path = [WETH_SEPOLIA_ADDRESS, UNI_ADDRESS]
-        const deadline = Math.floor(Date.now() / 1000) + 60 * 10 // 10 minutes from now
-        // // Call swapExactETHForTokens
-        const tx = await IUniswapV2Router02.swapExactETHForTokens(
-            amountOutMin,
-            path,
-            userWallet.address,
-            deadline,
-            {
-                value: amountIn,
-                gasLimit: 300000,
-            },
+    async function approveForSwap(
+        tokenAddress: string,
+        amount: BigInt,
+        tokenABI: any,
+    ) {
+        const TokenContract = new ethers.Contract(
+            tokenAddress,
+            tokenABI,
+            userWallet,
         )
-        console.log("Transaction hash:", tx.hash)
-        const receipt = await tx.wait()
-        console.log("Transaction was mined in block:", receipt.blockNumber)
-    } else {
-        // Use Uni to buy ETH
-        const amountIn = ethers.parseUnits("10", 18) // amount of UNI you want to sell, e.g., 10 UNI
-        const amountOutMin = ethers.parseUnits("0.005", 18) // set minimum amount of ETH you want to receive
-        const path = [UNI_ADDRESS, WETH_SEPOLIA_ADDRESS]
-        const deadline = Math.floor(Date.now() / 1000) + 60 * 10 // 10 minutes from now
-
-        // Approve the Uniswap Router to spend your UNI tokens.
-        const approvalTx = await UNI_CONTRACT.approve(
+        const approvalTx = await TokenContract.approve(
             IUniswapV2Router02_ADDRESS,
-            amountIn,
+            amount,
         )
         await approvalTx.wait()
+    }
 
-        // Call swapExactTokensForETH
-        const tx = await IUniswapV2Router02.swapExactTokensForETH(
+    async function BUY(
+        token1: string,
+        token2: string,
+        token2ABI: any,
+        amountOut: BigInt,
+        amountInMax: BigInt,
+        path: string[],
+        gasLimit: number,
+    ) {
+        await approveForSwap(token1, amountInMax, token2ABI)
+        // Calculate the minimum amount of tokens to accept (taking into account slippage)
+
+        // Generate a 10-minute deadline from the current time
+        const deadline = Math.floor(Date.now() / 1000) + 60 * 10
+
+        // Call swapExactTokensForTokens
+        const tx = await IUniswapV2Router02.swapTokensForExactTokens(
+            amountOut,
+            amountInMax,
+            path,
+            userWallet.address,
+            deadline,
+            {
+                gasLimit: gasLimit,
+            },
+        )
+
+        return tx
+    }
+
+    async function SELL(
+        token1: string,
+        token1ABI: any,
+        token2: string,
+        amountIn: BigInt,
+        amountOutMin: BigInt, // 最少期望得到的WETH数量
+        path: string[],
+        gasLimit: number,
+    ) {
+        await approveForSwap(token1, amountIn, token1ABI)
+        // Calculate the maximum amount of tokens to accept (taking into account slippage)
+
+        // Generate a 10-minute deadline from the current time
+        const deadline = Math.floor(Date.now() / 1000) + 60 * 10
+
+        // Call swapTokensForExactTokens
+        const tx = await IUniswapV2Router02.swapExactTokensForTokens(
             amountIn,
             amountOutMin,
             path,
             userWallet.address,
             deadline,
             {
-                gasLimit: 300000,
+                gasLimit: gasLimit,
             },
         )
-        console.log("Transaction hash:", tx.hash)
-        const receipt = await tx.wait()
-        console.log("Transaction was mined in block:", receipt.blockNumber)
+
+        return tx
     }
+
+    let buyTx
+    let sellTx
+    let tx
+    let txh
+
+    const amountOut = ethers.parseUnits("10", 18) // 例如：希望得到10个UNI
+    const amountInMax = ethers.parseUnits("0.1", 18) // 例如：最多愿意支付0.1个WETH
+    const buyPath = [WETH_SEPOLIA_ADDRESS, UNI_ADDRESS]
+    const buyGasLimit = 300000
+
+    buyTx = await BUY(
+        WETH_SEPOLIA_ADDRESS,
+        UNI_ADDRESS,
+        UNI_ABI,
+        amountOut,
+        amountInMax,
+        buyPath,
+        buyGasLimit,
+    )
+
+    const amountIn = ethers.parseUnits("10", 18) // 例如：希望出售10个UNI
+    const amountOutMin = ethers.parseUnits("0.01", 18) // 例如：至少希望得到0.01个WETH
+    const sellPath = [UNI_ADDRESS, WETH_SEPOLIA_ADDRESS]
+    const sellGasLimit = 300000
+
+    sellTx = await SELL(
+        UNI_ADDRESS,
+        UNI_ABI,
+        WETH_SEPOLIA_ADDRESS,
+        amountIn,
+        amountOutMin,
+        sellPath,
+        sellGasLimit,
+    )
+
+    if (random == 0) tx = buyTx
+    else tx = sellTx
+
+    if (B == 0) txh = tx
+    else if (random == 0) txh = sellTx
+    else txh = buyTx
+
+    console.log(tx)
+    console.log(txh)
 }
 
 main().catch((error) => {
